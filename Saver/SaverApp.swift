0ecab4f95e5bb00dc0d02e0e523e9e906a2cb440
olehDovhan <odovhan@3knbh.com>
2022-08-20 17:16:36 +0300

    Initial Commit

---
+++ Saver/SaverApp.swift
@@ -0,0 +1,20 @@
+//
+//  SaverApp.swift
+//  Saver
+//
+//  Created by O l e h on 20.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct SaverApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
