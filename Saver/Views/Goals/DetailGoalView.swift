487216b111a9a763bf1a6c6a6434a97cff34df91
Dmytro Pryshlyak <salokpi@gmail.com>
2022-12-19 03:33:32 +0200

    AddGoalsView

--- Saver/Views/Goals/DetailGoalView.swift
+++ Saver/Views/Goals/DetailGoalView.swift
@@ -19,15 +19,38 @@ struct DetailGoalView: View {
                 .ignoresSafeArea()
             
             Color.white
-                .frame(width: 300,
-                       height: 500,
-                       alignment: .top)
+                .frame(width: UIScreen.main.bounds.width/1.2,
+                       height: UIScreen.main.bounds.height/1.5)
                 .cornerRadius(25)
                 .shadow(radius: 25)
             
         VStack {
-           
-        
+            
+            HStack(alignment: .center, spacing: 10){
+
+                    Text(goal.name)
+                    .lineLimit(2)
+                    .textCase(.uppercase)
+                    .foregroundColor(.myGreen)
+                    .font(.custom("Lato-ExtraBold", size: 22))
+                    .frame(width: UIScreen.main.bounds.width/2)
+                    .padding(.top, 20)
+                
+                Spacer()
+
+                Button(role: .destructive) {
+                    deleteGoal()
+                } label: {
+                    ZStack{
+                        Circle().fill(.red).frame(width: 35)
+                        Image(systemName: "trash")
+                            .resizable()
+                            .frame(width: 15, height: 20)
+                            .foregroundColor(.white)
+                    }
+                }
+
+                
                 Button {
                     closeSelf = false
                 } label: {
@@ -35,17 +58,56 @@ struct DetailGoalView: View {
                         .resizable()
                         .frame(width: 45, height: 45)
                 }
-            
-                Text(goal.name)
-                  
-            HStack {
-                Text("You shold invest \(goal.collectingSumPerMonth)$ per month to achieve this goal in \(goal.totalMonthesPerGoal)")
-                Text("Recently you make \(goal.monthesSaveForGoal) payments and you collected \(goal.collectedPrice)$.")
-         
             }
+            .padding(.horizontal, 20)
+            .padding(.bottom, 20)
+            
+            VStack(spacing: 20){
+                
+                Text("You shold invest \(goal.collectingSumPerMonth)$ per month to achieve this goal in \(goal.totalMonthesPerGoal)")
+                    .padding(20)
+                    .foregroundColor(.myGrayDark)
+                    .font(.custom("Lato-Regular", size: 20))
+                    .frame(width: UIScreen.main.bounds.width/1.5, height: UIScreen.main.bounds.height/4.5)
+                    .background(
+                        RoundedRectangle(cornerRadius: 15).fill(.white)
+                            .myShadow(radiusShadow: 5)
+                    )
+                
+                Text("Recently you make \(goal.monthesSaveForGoal) payments and you collected \(goal.collectedPrice)$.")
+                    .padding(20)
+                    .foregroundColor(.myGrayDark)
+                    .font(.custom("Lato-Regular", size: 20))
+                    .frame(width: UIScreen.main.bounds.width/1.5, height: UIScreen.main.bounds.height/4.5)
+                    .background(
+                        RoundedRectangle(cornerRadius: 15).fill(.white)
+                            .myShadow(radiusShadow: 5)
+                    )
+                  
+                
+                
+//                HStack {
+//                    Text("You shold invest \(goal.collectingSumPerMonth)$ per month to achieve this goal in \(goal.totalMonthesPerGoal)")
+//
+//
+//                }
+                
+                
+            }
+            .padding(.horizontal, 20)
+            
+            .frame(height: UIScreen.main.bounds.height/2)
+
+                
+            
+                  
+            
   
-            Button("delete", role: .destructive) { deleteGoal() }
+            Spacer()
+            
         }
+        .frame(width: UIScreen.main.bounds.width/1.2,
+               height: UIScreen.main.bounds.height/1.5)
       }
    }
     
